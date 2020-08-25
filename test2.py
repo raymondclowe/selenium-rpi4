ceae3de165d51b8dec59e99c7f2e272cfe2f4fc6
Ramond The Incredible Lowe <raymondclowe@gmail.com>
2020-08-25 17:27:53 +0800

    test2.py goes headless

--- test2.py
+++ test2.py
@@ -1,12 +1,17 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.chrome.options import Options
 import time
 
-driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')  # Optional argument, if not specified will search path.
+options = webdriver.ChromeOptions()
+options.add_argument('--headless') 
+options.add_argument('--no-sandbox')
+
+driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver', options=options)  # Optional argument, if not specified will search path.
 driver.get('http://www.google.com/xhtml');
-time.sleep(5) # Let the user actually see something!
+time.sleep(3) # Let the user actually see something!
 search_box = driver.find_element_by_name('q')
 search_box.send_keys('ChromeDriver')
 search_box.submit()
-time.sleep(5) # Let the user actually see something!
+time.sleep(3) # Let the user actually see something!
+print(driver.title)
 driver.quit()
